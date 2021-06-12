f0313e35269f0af9ef07a0fdff8edef7dd096105
Amit Sides <amit@aramnik.com>
2021-06-13 00:08:42 +0300

    Changed welcome screen to refresh 3 times

--- welcome.py
+++ welcome.py
@@ -5,17 +5,22 @@ from arambadge import badge
 
 def show_welcome():
     display = badge.display
-    frame = displayio.Group()
 
-    pic = displayio.OnDiskBitmap(open("assets/welcome.bmp", "rb"))
-    grid = displayio.TileGrid(pic, pixel_shader=displayio.ColorConverter())
-    frame.append(grid)
+    for _ in range(3):
+        frame = displayio.Group()
 
-    pic = displayio.OnDiskBitmap(open("assets/banner.bmp", "rb"))
-    grid = displayio.TileGrid(pic, pixel_shader=displayio.ColorConverter())
-    frame.append(grid)
+        pic = displayio.OnDiskBitmap(open("assets/welcome.bmp", "rb"))
+        grid = displayio.TileGrid(pic, pixel_shader=displayio.ColorConverter())
+        frame.append(grid)
 
-    text_area = label.Label(terminalio.FONT, text="Welcome!", color=0x0, x=76, y=20)
-    frame.append(text_area)
+        pic = displayio.OnDiskBitmap(open("assets/banner.bmp", "rb"))
+        grid = displayio.TileGrid(pic, pixel_shader=displayio.ColorConverter())
+        frame.append(grid)
 
-    display.show(frame)
+        text_area = label.Label(terminalio.FONT, text="Welcome!", color=0x0, x=76, y=20)
+        frame.append(text_area)
+
+        display.show(frame)
+        display.refresh()
+        while display.time_to_refresh > 0:
+            pass
