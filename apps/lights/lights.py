82bdd6121fb64683fba5932c5730fa90d56edb9f
Lior Stern <liorst4@gmail.com>
2021-06-18 20:35:02 +0300

    lights: Draw instructions on screen.

--- apps/lights/lights.py
+++ apps/lights/lights.py
@@ -3,6 +3,9 @@ from adafruit_ble import BLERadio
 from adafruit_ble.advertising import Advertisement
 from adafruit_ble.advertising.standard import ProvideServicesAdvertisement
 from arambadge import badge
+import displayio
+import terminalio
+from adafruit_display_text import bitmap_label
 
 from apps.lights.ble_service import Service
 
@@ -26,8 +29,39 @@ class App:
             return True
         return False
 
+    def render_instruction_screen(self):
+        text = """--- Flashlight as a service ---
+
+Connect using BLE to {mac:X}.
+Send 6 bytes to {service_uid} to set the
+color of the LEDs.
+
+Press the action button to
+exit the application.""".format(
+            mac=int.from_bytes(self.ble.address_bytes, 'little'),
+            service_uid=str(self.service.uuid)
+        )
+        print(text)
+
+        screen = displayio.Group()
+        lines_group = displayio.Group(x=8, y=10)
+        for (index, line) in enumerate(text.splitlines()):
+            lines_group.append(bitmap_label.Label(
+                terminalio.FONT,
+                text=line[0:40],
+                color=0xffffff,
+                y = index * 12,
+                save_text=True
+            ))
+        screen.append(lines_group)
+        badge.display.show(screen)
+        while badge.display.time_to_refresh > 0:
+            pass
+        badge.display.refresh()
+
     def run(self):
         self.running = True
+        self.render_instruction_screen()
         while self.running:
             self.process_input()
             self.update()
