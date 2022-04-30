f1aa88c66763e1e220e10fb2497f785096038c00
Lior Stern <liorst4@gmail.com>
2022-04-30 16:40:13 +0300

    lights: Combine 2 files

--- apps/lights/lights.py
+++ apps/lights/lights.py
@@ -2,12 +2,31 @@ import time
 from adafruit_ble import BLERadio
 from adafruit_ble.advertising import Advertisement
 from adafruit_ble.advertising.standard import ProvideServicesAdvertisement
+from adafruit_ble.uuid import VendorUUID
+from adafruit_ble.characteristics.stream import StreamIn
 from arambadge import badge
+import adafruit_ble.services
 import displayio
 import terminalio
 from adafruit_display_text import bitmap_label
 
-from apps.lights.ble_service import Service
+class Service(adafruit_ble.services.Service):
+    uuid = VendorUUID(0xfeee)
+    _disp_rx = StreamIn(uuid=uuid, timeout=1.0, buffer_size=100)
+
+    def __init__(self):
+        super().__init__()
+
+    def update(self):
+        buffer = []
+        while self._disp_rx.in_waiting > 0:
+            byte = int.from_bytes(self._disp_rx.read(1), 'little')
+            buffer.append(byte)
+            if len(buffer) == 6:
+                badge.pixels[0] = tuple(buffer[0:3])
+                badge.pixels[1] = tuple(buffer[3:6])
+                buffer = []
+                time.sleep(1)
 
 class App:
     def __init__(self):
